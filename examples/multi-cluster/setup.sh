c41992b2ca5b1dc37ed37398b40285d19eab5372
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-10-16 13:14:51 +0300

    tidy: cleanup multi-cluster example

--- examples/multi-cluster/setup.sh
+++ examples/multi-cluster/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CLUSTER_NAME_PREFIX=${CLUSTER_NAME_PREFIX:-multi-cluster}
+KUBECONFIG_ONE=${KUBECONFIG_ONE:-cluster-one-kubeconfig.yaml}
+KUBECONFIG_TWO=${KUBECONFIG_TWO:-cluster-two-kubeconfig.yaml}
 
 set -ex
 
@@ -8,17 +10,17 @@ set -ex
 
 kind create cluster \
     --name ${CLUSTER_NAME_PREFIX}-one \
-    --config ./cluster-one.yaml \
-    --kubeconfig ./cluster-one.kubeconfig
-kubectl --kubeconfig ./cluster-one.kubeconfig \
-        config set-context --current --namespace kube-system
+    --kubeconfig ${KUBECONFIG_ONE} \
+    --config ./cluster-one.yaml
+
+kubectl --kubeconfig ${KUBECONFIG_ONE} config set-context --current --namespace kube-system
 
 kind create cluster \
     --name ${CLUSTER_NAME_PREFIX}-two \
-    --config ./cluster-two.yaml \
-    --kubeconfig ./cluster-two.kubeconfig
-kubectl --kubeconfig ./cluster-two.kubeconfig \
-        config set-context --current --namespace kube-system
+    --kubeconfig ${KUBECONFIG_TWO} \
+    --config ./cluster-two.yaml
+
+kubectl --kubeconfig ${KUBECONFIG_TWO} config set-context --current --namespace kube-system
 
 # Install each kubernetes configuration to the opposite cluster
 # In a real world situation this should be a kubeconfig with
@@ -26,17 +28,13 @@ kubectl --kubeconfig ./cluster-two.kubeconfig \
 # see the RBAC manifests in the bundle. These are the the objects
 # provided by the bundle in the storage-provider and the CNI APIs.
 
-kubectl --kubeconfig ./cluster-one.kubeconfig \
-        --namespace kube-system \
-        create secret generic cluster-two-credentials \
-        --from-file=kubeconfig=./cluster-two.kubeconfig
+kubectl --kubeconfig ${KUBECONFIG_ONE} --namespace kube-system \
+        create secret generic cluster-two-credentials --from-file=kubeconfig=${KUBECONFIG_TWO}
 
-kubectl --kubeconfig ./cluster-two.kubeconfig \
-        --namespace kube-system \
-        create secret generic cluster-one-credentials \
-        --from-file=kubeconfig=./cluster-one.kubeconfig
+kubectl --kubeconfig ${KUBECONFIG_TWO} --namespace kube-system \
+        create secret generic cluster-one-credentials --from-file=kubeconfig=${KUBECONFIG_ONE}
 
 # Install the CNI to both clusters
 
-kubectl kustomize deploy-one | kubectl --kubeconfig ./cluster-one.kubeconfig apply -f -
-kubectl kustomize deploy-two | kubectl --kubeconfig ./cluster-two.kubeconfig apply -f -
+kubectl kustomize deploy-one | kubectl --kubeconfig ${KUBECONFIG_ONE} apply -f -
+kubectl kustomize deploy-two | kubectl --kubeconfig ${KUBECONFIG_TWO} apply -f -
--- examples/multi-cluster/teardown.sh
+++ examples/multi-cluster/teardown.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 CLUSTER_NAME_PREFIX=${CLUSTER_NAME_PREFIX:-multi-cluster}
+KUBECONFIG_ONE=${KUBECONFIG_ONE:-cluster-one-kubeconfig.yaml}
+KUBECONFIG_TWO=${KUBECONFIG_TWO:-cluster-two-kubeconfig.yaml}
 
 set -ex
 
 kind delete cluster --name ${CLUSTER_NAME_PREFIX}-one
 kind delete cluster --name ${CLUSTER_NAME_PREFIX}-two
-rm -f cluster-one.kubeconfig cluster-two.kubeconfig
+rm -f ${KUBECONFIG_ONE} ${KUBECONFIG_TWO}
+
